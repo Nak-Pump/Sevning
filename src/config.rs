885d1f11cc68f6c9f7d5d8a86598be53a323882b
Coaixy <Coaixy@outlook.com>
2024-09-24 16:41:16 +0800

    Added Toml configuration read

--- src/config.rs
+++ src/config.rs
@@ -2,38 +2,69 @@ use std::collections::HashMap;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
-struct MainConfig {}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct ApplicationNode {
-    ip: String,
-    port: String,
-    verify: bool,
-    password: String,
+pub struct MainConfig {
+    pub application: ApplicationNode,
+    pub runtimes: RuntimeNode,
+    pub users: UserNode,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct RuntimeNode {
-    command_map: HashMap<String, String>,
+pub struct ApplicationNode {
+    pub ip: String,
+    pub port: String,
+    pub verify: bool,
+    pub password: String,
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-struct UserNode {
-    user_map: HashMap<String, String>,
+pub struct RuntimeNode {
+    #[serde(flatten)]
+    pub command_map: HashMap<String, String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct UserNode {
+    #[serde(flatten)]
+    pub user_map: HashMap<String, String>,
 }
 
 pub struct SConfig {
     pub path: String,
+    pub config: MainConfig,
 }
+
+fn get_default_config() -> MainConfig {
+    MainConfig {
+        application: ApplicationNode {
+            ip: String::from("127.0.0.1"),
+            port: String::from("10430"),
+            verify: false,
+            password: String::from(""),
+        },
+        runtimes: RuntimeNode {
+            command_map: HashMap::new(),
+        },
+        users: UserNode {
+            user_map: HashMap::new(),
+        },
+    }
+}
+
 
 impl Default for SConfig {
     fn default() -> Self {
         let current_dir = std::env::current_dir();
         if let Ok(current_dir) = current_dir {
             let path = current_dir.to_str().unwrap().to_string();
-            SConfig { path }
+            SConfig {
+                path,
+                config: get_default_config(),
+            }
         } else {
-            SConfig { path: ".".to_string() }
+            SConfig {
+                path: "".to_string(),
+                config: get_default_config(),
+            }
         }
     }
 }
@@ -42,6 +73,27 @@ impl SConfig {
         SConfig::default()
     }
     pub fn with_path(path: String) -> Self {
-        SConfig { path }
+        SConfig {
+            path,
+            config: get_default_config(),
+        }
+    }
+
+    pub fn init(self: &mut Self) {
+        let path = format!("{}/Sevning.toml", self.path);
+        let config_text = std::fs::read_to_string(path);
+        match config_text {
+            Ok(config_text) => {
+                let config = toml::from_str(&config_text);
+                if let Ok(config) = config {
+                    self.config = config;
+                } else {
+                    panic!("Failed to parse config.toml");
+                }
+            }
+            Err(_) => {
+                panic!("Failed to load config.toml");
+            }
+        }
     }
 }

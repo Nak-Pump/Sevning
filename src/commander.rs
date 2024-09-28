a7615b836eced00df98f28937f5077adcd364cd7
Coaixy <Coaixy@outlook.com>
2024-09-28 22:22:09 +0800

    add mpsc with command and web

--- src/commander.rs
+++ src/commander.rs
@@ -4,6 +4,7 @@ use std::thread::sleep;
 use std::time::Duration;
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::UnboundedReceiverStream;
+use tracing_subscriber::fmt;
 
 pub struct Commander {
     pub command_owner: String,
@@ -11,48 +12,52 @@ pub struct Commander {
     pub command_args: Vec<String>,
     pub start_time: Duration,
     pub end_time: Duration,
-    pub rx: Option<UnboundedReceiverStream<String>>,
     pub uuid: String,
+    pub latest_text: String,
+    pub status: i32,
 }
 
 impl Default for Commander {
     fn default() -> Self {
-        let (_tx, rx) = mpsc::unbounded_channel();
         Commander {
             command_owner: String::from(""),
             command_name: String::from(""),
             command_args: Vec::new(),
             start_time: Duration::new(0, 0),
             end_time: Duration::new(0, 0),
-            rx: Some(UnboundedReceiverStream::new(rx)),
             uuid: String::from(""),
+            latest_text: String::from(""),
+            status: -1,
         }
     }
 }
 
 impl Commander {
     pub fn new(command_owner: String, command_name: String, command_args: Vec<String>) -> Self {
-        let (_tx, rx) = mpsc::unbounded_channel();
         let uuid = uuid::Uuid::new_v4().to_string();
         let start_time = Duration::new(0, 0);
         let end_time = Duration::new(0, 0);
+        let latest_text = String::from("");
         Commander {
             command_owner,
             command_name,
             command_args,
             start_time,
             end_time,
-            rx: Some(UnboundedReceiverStream::new(rx)),
             uuid,
+            latest_text,
+            status: -1,
         }
     }
 
-    pub fn create_new_command(&self) {
+    pub async fn create_new_command(&mut self,tx: mpsc::Sender<String>) {
         let mut command = Command::new(&self.command_name)
             .args(&self.command_args)
             .stdout(Stdio::piped())
             .spawn()
             .expect("Failed to start command");
+
+        println!("Command: {:?} {:?}", self.command_name, self.command_args);
 
         let stdout = command.stdout.take().expect("Failed to capture stdout");
         let mut reader = BufReader::new(stdout);
@@ -64,7 +69,9 @@ impl Commander {
             match reader.read_line(&mut buffer) {
                 Ok(0) => break,
                 Ok(_) => {
-                    print!("{}", buffer);
+                    if tx.send(buffer.clone()).await.is_err() {
+                        break;
+                    }
                     std::io::stdout().flush().unwrap();
                 }
                 Err(err) => {
@@ -72,10 +79,15 @@ impl Commander {
                     break;
                 }
             }
-            sleep(Duration::from_millis(100));
+            tokio::time::sleep(Duration::from_millis(500)).await;
         }
 
         let status = command.wait().expect("Failed to wait on child");
-        println!("Command exited with status: {}", status);
+        self.status = status.code().unwrap_or(-1);
+        tx.send("state_exit".to_string()).await.unwrap()
+    }
+
+    pub fn get_command_output(&self) -> String {
+        self.latest_text.clone()
     }
 }

d5a40e322a87ff75cd8e36f86d9b545f0f15ad2e
Coaixy <Coaixy@outlook.com>
2024-09-30 10:27:28 +0800

    change singleton commander type

--- src/singleton.rs
+++ src/singleton.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
-use std::sync::Mutex;
+use std::sync::{Arc};
 use once_cell::sync::Lazy;
+use tokio::sync::Mutex;
 use crate::commander::Commander;
 
 struct CommandManager {
-    command_map: HashMap<String, Commander>,
+    command_map: HashMap<String, Arc<Mutex<Commander>>>,
 }
 
 impl CommandManager {
@@ -14,15 +15,15 @@ impl CommandManager {
         }
     }
 
-    fn add_command(&mut self, command: Commander) {
-        self.command_map.insert(command.uuid.clone(), command);
+    async fn add_command(&mut self, command: Arc<Mutex<Commander>>) {
+        self.command_map.insert(command.lock().await.uuid.clone(), command);
     }
 
     fn remove_command(&mut self, uuid: String) {
         self.command_map.remove(&uuid);
     }
 
-    fn get_command(&self, uuid: String) -> Option<&Commander> {
+    fn get_command(&self, uuid: String) -> Option<&Arc<Mutex<Commander>>> {
         self.command_map.get(&uuid)
     }
 }

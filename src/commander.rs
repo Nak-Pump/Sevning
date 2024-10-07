0c6b9891d4b18de43b32f45dcccd36e8c0d2e0ba
Coaixy <Coaixy@outlook.com>
2024-10-07 15:43:56 +0800

    change time type

--- src/commander.rs
+++ src/commander.rs
@@ -2,18 +2,19 @@ use std::io::{BufRead, BufReader, Write};
 use std::process::{Command, Stdio};
 use std::thread::sleep;
 use std::time::Duration;
+use chrono::Local;
 use tokio::sync::mpsc;
 use tokio_stream::wrappers::UnboundedReceiverStream;
 use tracing_subscriber::fmt;
+use crate::singleton::COMMAND_MANAGER;
 
 pub struct Commander {
     pub command_owner: String,
     pub command_name: String,
     pub command_args: Vec<String>,
-    pub start_time: Duration,
-    pub end_time: Duration,
+    pub start_time: String,
+    pub end_time: String,
     pub uuid: String,
-    pub latest_text: String,
     pub status: i32,
 }
 
@@ -23,10 +24,9 @@ impl Default for Commander {
             command_owner: String::from(""),
             command_name: String::from(""),
             command_args: Vec::new(),
-            start_time: Duration::new(0, 0),
-            end_time: Duration::new(0, 0),
+            start_time: String::from(""),
+            end_time: String::from(""),
             uuid: String::from(""),
-            latest_text: String::from(""),
             status: -1,
         }
     }
@@ -35,9 +35,8 @@ impl Default for Commander {
 impl Commander {
     pub fn new(command_owner: String, command_name: String, command_args: Vec<String>) -> Self {
         let uuid = uuid::Uuid::new_v4().to_string();
-        let start_time = Duration::new(0, 0);
-        let end_time = Duration::new(0, 0);
-        let latest_text = String::from("");
+        let start_time = String::from("");
+        let end_time = String::from("");
         Commander {
             command_owner,
             command_name,
@@ -45,18 +44,17 @@ impl Commander {
             start_time,
             end_time,
             uuid,
-            latest_text,
             status: -1,
         }
     }
 
-    pub async fn create_new_command(&mut self,tx: mpsc::Sender<String>) {
+    pub async fn create_new_command(&mut self, tx: mpsc::Sender<String>) {
         let mut command = Command::new(&self.command_name)
             .args(&self.command_args)
             .stdout(Stdio::piped())
             .spawn()
             .expect("Failed to start command");
-
+        self.start_time = Local::now().to_string();
         println!("Command: {:?} {:?}", self.command_name, self.command_args);
 
         let stdout = command.stdout.take().expect("Failed to capture stdout");
@@ -84,10 +82,8 @@ impl Commander {
 
         let status = command.wait().expect("Failed to wait on child");
         self.status = status.code().unwrap_or(-1);
+        COMMAND_MANAGER.lock().await.set_statue(self.uuid.clone(), self.status).await;
+        self.end_time = Local::now().to_string();
         tx.send("state_exit".to_string()).await.unwrap()
-    }
-
-    pub fn get_command_output(&self) -> String {
-        self.latest_text.clone()
     }
 }
